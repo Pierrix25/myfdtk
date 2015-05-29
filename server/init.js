502fd6b27e45d655f9c79dc96e0a22ce9aff62e1
Pierrix25 <pferrent@gmail.com>
2015-05-29 18:07:05 +0200

    0.1.1.2
    Avec images affichées

--- server/init.js
+++ server/init.js
@@ -2,17 +2,29 @@ Meteor.startup(function () {
   UploadServer.init({
     tmpDir: 'c:/meteor/myfdtk/.uploads/tmp',
     uploadDir: 'c:/meteor/myfdtk/.uploads/',
+	maxFileSize: 5000000,
 	getFileName: function(file, formData) {
-      return formData.nom+'_'+formData.prefix+'_'+formData.id+'.'+file.name.split('.').pop();
+      return formData.nom+'_'+formData.prefix+'_'+file.name.split('.').shift()+'_'+formData.id+'.'+file.name.split('.').pop();
     },
     finished: function(file, formData) {
-      console.log('Write to database: ' + file);
-	  if (formData='Menu') {
-			FoodTrucks.update({_id : formData.id}, { $push: { Menu: file }});
+		console.log('==>file.name='+file.name);
+		console.log('==>file.url='+file.url);
+		console.log('==>prefix='+formData.prefix);
+	  if (formData.prefix=='Menu') {
+			console.log('pavé menu');
+		console.log('prefix='+formData.prefix);
+			FoodTrucks.update({_id : formData.id}, { $set: { menu: file }});
 		}
-	  if (formData='Logo') {
-			FoodTrucks.update({_id : formData.id}, { $push: { Logo: file }});
+	  if (formData.prefix=='Logo') {
+			console.log('pavé logo');
+		console.log('prefix='+formData.prefix);
+			FoodTrucks.update({_id : formData.id}, { $set: { logo: file }});
 		}
-    }
+ 	  if (formData.prefix=='Truck') {
+			console.log('pavé truck');
+		console.log('prefix='+formData.prefix);
+			FoodTrucks.update({_id : formData.id}, { $set: { truck: file }});
+		}
+   }
   })
 });
