0dcfd56ec260ceb39f6cdc02a977324aa3bba0b8
Pierrix25 <pferrent@gmail.com>
2016-03-18 21:20:08 +0100

    Avec Upload et update sans carte
    La version précédente perdait les images en cas de modification de
    truck. Cette version répare ce bug.

--- client/views/aFoodTruck.js
+++ client/views/aFoodTruck.js
@@ -1,30 +1,43 @@
 Template.aFoodTruck.events({
 	'submit form': function(e){
         e.preventDefault();
-//  console.log('aFoodTruck event !!!!!');
+  console.log('aFoodTruck event !!!!!');
         
         var nom = $("input[name='nom']").val();
         var theme = $("input[name='theme']").val();
         var description = $("textarea[name='description']").val();
         var lon = Session.get('myLon');
         var lat = Session.get('myLat');
-        
+	if (myFdTk) {
+        var truck = myFdTk.truck;
+        var menu = myFdTk.menu;
+        var logo = myFdTk.logo;
+} else {
+        var truck = '';
+        var menu = '';
+        var logo = '';
+}
         var foodtruck = {
                 nom: nom,
                 theme: theme,
                 description: description,
                 lon: lon,
                 lat: lat,
+			truck: truck,
+			menu: menu,
+			logo: logo,
 			truckerId: Meteor.userId()
         }
 		if (myFdTk) {
 			FoodTrucks.update({_id : myFdTk._id}, foodtruck, {upsert: true, multi: false,} );
 			Session.set('signUp-visible', true);
 			Session.set('majOk', true);
+			console.log('update');
 		} else {
 			FoodTrucks.insert(foodtruck);
 			Session.set('signUp-visible', true);
 			Session.set('majOk', true);
+			console.log('insert');
 		}
     }
 });
